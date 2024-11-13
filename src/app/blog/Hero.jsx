7dcb866ae7c7876b50c774d8baad621c9994bfa5
Shahwar786 <shahwarasif899@gmail.com>
2024-11-13 23:40:11 +0600

    Blog Page Added

---
+++ src/app/blog/Hero.jsx
@@ -0,0 +1,8 @@
+
+export default function Hero() {
+    return (
+        <div className="flex flex-col items-center gap-y-8 bg-[#ECEDF0] 2xl:h-[70vh] justify-center relative py-14">
+            <h1 className="font-bold leading-[1.1] text-[27px] md:text-[35px] lg:text-[40px] xl:text-[50px] 2xl:text-[68px]">Our Newsroom</h1>
+        </div>
+    );
+}
---
+++ src/app/blog/page.jsx
@@ -0,0 +1,11 @@
+import Blog from "@/components/Blog";
+import Hero from "./Hero";
+
+export default function page() {
+    return (
+        <div>
+            <Hero />
+            <Blog />
+        </div>
+    );
+}
