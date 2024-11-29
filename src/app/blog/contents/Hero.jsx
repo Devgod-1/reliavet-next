dd065c7cd7973fe1dd639fbe29ba1dbcacb80a8f
brighter <brighter0611@gmail.com>
2024-11-28 21:23:41 -0500

    hot fix

--- src/app/blog/contents/Hero.jsx
+++ src/app/blog/contents/Hero.jsx
@@ -128,7 +128,7 @@ export default function Hero() {
                         >
                           {blog.title.rendered}
                         </h2>
-                        <p className="md:h-48 sm:h-24 text-ellipsis overflow-hidden text-xs lg:text-sm 2xl:text-base text-gray-600" dangerouslySetInnerHTML={{ __html: blog?.content?.rendered }}></p>
+                        <p className="md:h-48 h-24 text-ellipsis overflow-hidden text-xs lg:text-sm 2xl:text-base text-gray-600" dangerouslySetInnerHTML={{ __html: blog?.content?.rendered }}></p>
                       </div>
                       <div className="flex justify-between items-end gap-3 mt-6">
                         <div className="relative flex items-center space-x-3">
