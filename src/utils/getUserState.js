48335ddac022b693425bd63b6da9f16357dd1281
paul <phreshdev@gmail.com>
2024-12-04 10:09:04 -0400

    fix the issue - update the image width of the provider

--- src/utils/getUserState.js
+++ src/utils/getUserState.js
@@ -17,8 +17,7 @@ export const getUserState = async (latitude, longitude) => {
         )?.short_name;
 
         if (state) {
-            // return state; // Return the state as soon as it's found
-            return 'SC';
+            return state; // Return the state as soon as it's found
         }
     }
 
