ed2d94890bbfaf49eb29126370e898db4267fc1d
paul <phreshdev@gmail.com>
2025-01-29 13:52:44 -0400

    add the OG tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,23 @@ import "swiper/css/pagination";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ContactUs from "@/components/ContactUs";
-
+import Head from "next/head";
 
 export const metadata = {
   title: "ReliaVet | Schedule Virtual Vet Appointment Online",
   description: "Reliable vets for you and your pets!",
+  openGraph: {
+    title: "ReliaVet | Schedule Virtual Vet Appointment Online",
+    description: "Reliable vets for you and your pets!",
+    url: "https://reliavet.com",
+    siteName: "ReliaVet",
+    type: "website",
+  },
+  twitter: {
+    card: "Reliavet",
+    title: "ReliaVet | Schedule Virtual Vet Appointment Online",
+    description: "Reliable vets for you and your pets!",
+  },
 };
 
 export default async function RootLayout({ children }) {
