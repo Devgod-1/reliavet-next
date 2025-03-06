471536ab577b59ad1d4dcf69184467cc84c4bf3e
paul <phreshdev@gmail.com>
2025-03-06 15:15:55 -0400

    add the code for the sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://reliavet.com', // Replace with your domain
+    generateRobotsTxt: true, // Generate a robots.txt file
+    sitemapSize: 5000, // Limit the number of URLs per sitemap file
+};
